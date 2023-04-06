deb7a8bed09551a1f12a4ebf84d1556d13345ec5
Sumant Singh <singhsumant581@gmail.com>
2023-04-06 13:25:47 +0530

    made some changes in RBS

--- binary search/src/RotatedBinarySearch.java
+++ binary search/src/RotatedBinarySearch.java
@@ -51,39 +51,41 @@ public class RotatedBinarySearch {
             int end = arr.length - 1;
             while(start<=end){
                 int mid = start + (end-start)/2 ;
+
                 if(mid<end && arr[mid]>arr[mid+1])
                 return mid;
 
                 if(mid>start && arr[mid]<arr[mid-1])
                 return mid-1;
 
-                //if elements at middle ,start,end are equal then skip the duplicates
+                //if elements at middle , start, and end are equal then just skip the duplicates
                 if(arr[mid]==arr[start] && arr[mid]==arr[end]){
-                    //skip the duplicates
-                    //NOTE: what if these elements at start and end were the pivot??
+
+                    //NOTE:what if these elements at start and end are pivots??
+
                     //check if start is pivot
                     if(arr[start]>arr[start+1]){
                         return start;
                     }
                     start++;
+
                     //check if end is pivot
                     if(arr[end]<arr[end-1]){
                         return end-1;
                     }
                     end--;
                 }
+                //left side is sorted so pivot should be right.
 
-                //left side is sorted , so pivot should be on right side.
-
-                else if(arr[start]<arr[mid] || (arr[start]==arr[mid])&&arr[mid]>arr[end]){
-                    start=mid+1;
+                else if(arr[start]<arr[mid] || (arr[start]==arr[mid] && arr[end]<arr[mid]) ){
+                     start = mid +1;
                 }
                 else{
-                    end = mid-1;
+                    end = mid -1;
                 }
                 
             }
-            return -1; 
+            return -1;  
         }
         
 //this will not work for duplicate values in array
