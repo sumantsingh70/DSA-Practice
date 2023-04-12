33f0110eb2ba2fa5501ff6a483b23e8cb81c994c
Sumant Singh <singhsumant581@gmail.com>
2023-04-12 18:08:22 +0530

    Binary Search in Completely sorted matrix(not-complete)

---
+++ binary search/src/SortedMatrix.java
@@ -0,0 +1,22 @@
+public class SortedMatrix {
+    public static void main(String[] args) {
+        
+    }
+
+    static int[] BinarySearch(int[] matrix,int row,int cStart,int cEnd,int target){
+        while(cStart<=cEnd){
+            int mid = cStart+(cEnd-cStart)/2;
+            if()
+        }
+    }
+
+    static int[] search(int[][] matrix,int target){
+        int rows = matrix.length;
+        int cols = matrix[0].length;  //be cautious matrix may be empty
+
+        if(rows==1){
+
+        }
+    }
+    
+}
